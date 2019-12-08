3e6de8f86fcbed00639f808a166c2e66d0428f0e
Emeka Augustine <cafafanscoders@gmail.com>
2019-12-08 21:03:02 +0800

    Add Templates

--- BlogService/Blog/views.py
+++ BlogService/Blog/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>Welcome To The Blog Home Page!</h1>')
+    return render(request, 'home.html')
 
 def about(request):
     return HttpResponse('<h1>Welcome To About Page!</h1>')
