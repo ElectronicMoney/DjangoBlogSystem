7f4f70bf19cbbc9277afa475efed84e028d80a57
Emeka Augustine <cafafanscoders@gmail.com>
2019-12-08 20:29:48 +0800

    Add About route

--- BlogService/Blog/views.py
+++ BlogService/Blog/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse('<h1>Welcome To The Blog Home Page!</h1>')
+
+def about(request):
+    return HttpResponse('<h1>Welcome To About Page!</h1>')
--- BlogService/BlogService/settings.py
+++ BlogService/BlogService/settings.py
@@ -74,6 +74,15 @@ WSGI_APPLICATION = 'BlogService.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     # 'ENGINE': 'django.db.backends.mysql',
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'blog',
+    #     'USER': 'root',
+    #     'PASSWORD': 'Tessy@38385763',
+    #     'HOST': '127.0.0.1',
+    #     'PORT': '3306',
+    # }
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
